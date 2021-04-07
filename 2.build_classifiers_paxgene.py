82d6f5fd06d8d53764e1e3dadcc523cb8efbaffe
Lucile Neyton <lucile.neyton@gmail.com>
2021-04-07 11:10:34 -0700

    add paxgene script

---
+++ 2.build_classifiers_paxgene.py
@@ -0,0 +1,212 @@
+'''
+2.build_classifiers_paxgene.py
+created on April 6 2021
+lucile.neyton@ucsf.edu
+
+This script aims at building classifiers to allocate samples to one of the two
+groups (sepsis vs non-sepsis) given gene expression values generated from PAXgene tubes.
+The final models are tested on a held-out set.
+
+Input files (data folder):
+    - CSV-formatted file containing raw PAXgene
+        gene counts (samples x genes)
+    - CSV-formatted file containing sepsis labels
+        (samples x)
+    - CSV-formatted file containing
+        the list of differentially expressed genes (genes x)
+
+Output files (results folder):
+    - One dump file per classifier
+'''
+
+# load libraries and functions
+#
+import sys
+sys.path.append("/Users/lucileneyton/OneDrive - University of California, San Francisco/UCSF/EARLI_VALID/de_analysis")
+#
+from custom_classes import VstTransformer, CustomRFECV, CustomBaggingClassifier, DGEA_filter
+#
+from itertools import product
+#
+import os
+# 1.19.1
+import numpy as np
+# 1.1.3
+import pandas as pd
+# 0.23.2
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import LinearSVC
+# 1.2.1
+from xgboost import XGBClassifier
+# 0.17.0
+from joblib import dump, load
+
+# set paths
+data_path = "/Users/lucileneyton/OneDrive - University of California, San Francisco/UCSF/EARLI_plasma/data/"
+paxgene_data_path = "/Users/lucileneyton/OneDrive - University of California, San Francisco/UCSF/EARLI_VALID/data/"
+results_path = "/Users/lucileneyton/OneDrive - University of California, San Francisco/UCSF/EARLI_plasma/results/"
+
+# set params
+mode_ = "create"
+
+# list parameter values
+# sample filter / gene filter / DE FDR / covariates / ratios / algo
+comb_list = [('50000', '40', '0.1', 'FALSE', 'FALSE', 'xgb'),
+             ('50000', '20', '0.1', 'FALSE', 'FALSE', 'bsvm'),
+             ('50000', '30', '0.1', 'TRUE', 'TRUE', 'bsvm')]
+
+for comb_ in comb_list:
+    results_prefix = comb_[0] + "_" + comb_[1] + "_" + comb_[2] + "_" + comb_[3]
+    ratios_ = comb_[4]
+    algo_ = comb_[5]
+
+    print("=====")
+    print(results_prefix)
+    print("=====")
+
+    #########################
+    # DATA LOADING
+    #########################
+    # paxgene data
+    cnt_data = pd.read_csv(data_path + "raw/" + "EARLI_star_pc_and_lincRNA_genecounts.qc.tsv", index_col=0, sep="\t")
+
+    # group labels
+    paxgene_meta_data = pd.read_csv(
+        paxgene_data_path + "processed/" + "EARLI_metadata_adjudication_IDseq_LPSstudyData_7.5.20.csv")
+
+    meta_data = pd.read_csv(data_path + "processed/" + results_prefix + "_metadata_1vs4.csv", index_col=0)
+    dgea_results = pd.read_csv(results_path + results_prefix + "_DGEA_results.csv", index_col=0)
+
+    #########################
+    # DATA PREPROCESSING
+    #########################
+    # keep only samples of interest (groups 1 and 4, from the metadata file)
+    paxgene_meta_data["EARLI_Barcode"] = ["EARLI_" + str(x) for x in paxgene_meta_data["Barcode"]]
+    paxgene_meta_data = paxgene_meta_data.loc[(paxgene_meta_data.EARLI_Barcode.isin(meta_data.SampleID)), :]
+    cnt_data = cnt_data.loc[:, cnt_data.columns.isin(paxgene_meta_data.HOST_PAXgene_filename)]
+
+    # format count data column and row names
+    cnt_data.columns = [x.split("_")[0] + "_" + x.split("_")[1] for x in cnt_data.columns]
+    cnt_data.index = [x.split(".")[0] for x in cnt_data.index]
+
+    # keep only DE genes
+    cnt_data = cnt_data.loc[dgea_results.index.values, :]
+    cnt_data.index = dgea_results.hgnc_symbol.values
+
+    # drop duplicate gene symbols
+    cnt_data = cnt_data.loc[~cnt_data.index.duplicated(), :]
+
+    # filter samples not in the meta_data file and make sure vst_data and meta_data have the same rows order
+    cnt_data = cnt_data.T
+    cnt_data = cnt_data.reindex(meta_data.SampleID.values).dropna(axis=0, how='any')
+
+    # split data into train and test sets
+    cnt_data_train, cnt_data_test, sepsis_cat_train, sepsis_cat_test = train_test_split(cnt_data,
+                                                                                        paxgene_meta_data.Group,
+                                                                                        test_size=0.3,
+                                                                                        random_state=123,
+                                                                                        stratify=paxgene_meta_data.Group)
+
+    # store variable labels
+    name_vars = cnt_data_train.columns
+
+    # store sample ids
+    name_samps_train = cnt_data_train.index
+    name_samps_test = cnt_data_test.index
+
+    # apply vst to counts
+    vst_transformer = VstTransformer()
+    vst_transformer = vst_transformer.fit(cnt_data_train)
+    vst_data_train = vst_transformer.transform(cnt_data_train)
+    vst_data_test = vst_transformer.transform(cnt_data_test)
+
+    if ratios_:
+        # build ratio objects
+        vst_data_train = build_ratios_df(vst_data_train, name_vars, name_samps_train)
+        vst_data_test = build_ratios_df(vst_data_test, name_vars, name_samps_test)
+    else:
+        # apply standard scaling to vst datasets
+        standard_scaler = StandardScaler()
+        standard_scaler = standard_scaler.fit(vst_data_train)
+        vst_data_train = standard_scaler.transform(vst_data_train)
+        vst_data_test = standard_scaler.transform(vst_data_test)
+
+    if (algo_ == "xgb"):
+        #########################
+        # XGBOOST - CV within a CV for grid search and RFE as part of a pipeline
+        # n_jobs should be set to 1/None (or at least not -1 in both) to avoid nested parallelism
+        #########################
+        rfecv = CustomRFECV(estimator=XGBClassifier(random_state=123, nthread=1), cv=5, n_jobs=1,
+                            scoring='roc_auc', step=0.1,
+                            min_features_to_select=2, verbose=True, max_features=100)
+
+        # pipeline steps
+        pipe = Pipeline([('rfe', rfecv), ('xgbc', XGBClassifier())])
+
+        # create the parameter grid
+        param_grid = {
+            'xgbc__random_state': [123],
+            'xgbc__nthread': [1],
+            'xgbc__learning_rate': [0.01, 0.1, 0.3],  # boosting learning rate 0 to 1, default 0.3
+            'xgbc__objective': ['binary:logistic'],  # learning objective
+            'xgbc__max_depth': [10, 50, 100],  # max number of levels in each tree
+            'xgbc__n_estimators': [100, 1000]  # number of trees
+        }
+
+    else:
+        if algo_ == "bsvm":
+            #########################
+            # BagSVM - CV within a CV for grid search and RFE as part of a pipeline
+            # n_jobs should be set to 1/None (or at least not -1 in both) to avoid nested parallelism
+            #########################
+            rfecv = CustomRFECV(estimator=CustomBaggingClassifier(base_estimator=LinearSVC(max_iter=10000),
+                                                                  random_state=123),
+                                cv=5, n_jobs=1, scoring='roc_auc',
+                                step=0.1, min_features_to_select=2, verbose=True, max_features=100)
+
+            # pipeline steps
+            pipe = Pipeline([('rfe', rfecv),
+                             ('bsvmc', CustomBaggingClassifier(base_estimator=LinearSVC(max_iter=10000)))])
+
+            # create the parameter grid
+            param_grid = {
+                'bsvmc__random_state': [123],
+                'bsvmc__base_estimator__C': [0.01, 0.1, 1, 10],  # regularisation parameter
+                'bsvmc__max_features': [0.1, 0.5, 0.8],  # max number of features to draw (without replacement)
+                'bsvmc__n_estimators': [100, 1000]  # number of base estimators
+            }
+
+    # add ratio prefix
+    if ratios_:
+        algo_ = "ratios_" + algo_
+
+    # set the grid search object
+    search = GridSearchCV(estimator=pipe, cv=5, n_jobs=1, scoring='roc_auc', param_grid=param_grid,
+                          verbose=True)
+
+    if mode_ == "create":
+        # fit the chosen model
+        search.fit(vst_data_train, sepsis_cat_train)
+        dump(search, results_path + results_prefix + "_paxgene" + "_dump_" + algo_ + "_de_genes.joblib")
+    else:
+        if mode_ == "load":
+            search = load(results_path + results_prefix + "_paxgene" + "_dump_" + algo_ + "_de_genes.joblib")
+
+    print(search.best_params_)
+    print(search.best_score_)
+    print(name_vars[search.best_estimator_.named_steps["rfe"].support_])
+
+    # save list of predictors
+    best_vars = name_vars[search.best_estimator_.named_steps["rfe"].support_]
+    pd.DataFrame(best_vars).to_csv(results_path + results_prefix + "_paxgene" + "_best_vars_" + algo_ + "_de_genes.csv",
+                                   header=False,
+                                   index=False)
+
+    # evaluate on test data
+    probs = search.predict_proba(vst_data_test)
+    probs = probs[:, 1]
+    roc_auc = roc_auc_score(sepsis_cat_test, probs)
+    print(roc_auc)
